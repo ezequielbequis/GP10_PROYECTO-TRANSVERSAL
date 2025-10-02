e81939d314c53a4921fa1c7a242253fdba2fbf22
NataliaDave <nataliadave@gmail.com>
2025-10-02 20:21:18 -0300

    Update Main GP10_Universidad

--- src/vistas/GP10_universidad.java
+++ src/vistas/GP10_universidad.java
@@ -1,19 +1,47 @@
 package vistas;
 
+import entidades.Alumno;
+import java.sql.Connection;
+import java.time.LocalDate;
+import persistencia.miConexion;
+//import persistencia.ConexionS;
+
 
 /**
  *
- * @author matia
+ * @author Grupo10 
+ * Altamirano Karina
+ * Gianfranco Antonacci Matías
+ * Bequis Marcos Ezequiel
+ * Dave  Natalia
+ * Quiroga Dorzan Alejo
  */
 public class GP10_universidad {
-
+private AlumnoData alumnoData;    
+private miConexion conexion; 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        LocalDate fecha= LocalDate.now();
+        Alumno alum = new Alumno (33465789, "Altamirano" , "Karina", LocalDate.now(), true);
+        new GP10_universidad().conectar (alum);
+        System.out.println("Alumno " + alum.getNombre() + " guardado con éxito");
+        
     }
     
+    void conectar (Alumno alum){
     
+      //  Connection c2= ConexionS.buscarConexion();
+        
+        conexion = new miConexion ("jdbc:mariadb://localhost/gp10_ulp", "root", ""); // constructor
+        alumnoData = new AlumnoData (conexion); //alumno
+        alumnoData.guardarAlumno(alum); //persistencia
+        Alumno alu  = alumnoData.buscarAlumno(alum.getId());
+        System.out.println("Datos: " + alu);
+    
+    }
     
 }
