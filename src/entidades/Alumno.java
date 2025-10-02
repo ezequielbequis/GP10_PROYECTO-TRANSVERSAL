889bfc19c3366830d4ccce9949764dec877f3056
Matias Antonacci <matiasgantonacci@gmail.com>
2025-10-02 19:24:19 -0300

    Clase Alumno ( atributos y metodos )

--- src/entidades/Alumno.java
+++ src/entidades/Alumno.java
@@ -7,5 +7,71 @@ import java.time.LocalDate;
  * @author matia
  */
 public class Alumno {
-    
+    private int idAlumno = -1;
+    private int dni;
+    private String apellido;
+    private String nombre;
+    private LocalDate fechaNac;
+    private boolean estado;
+
+    public Alumno(int dni, String apellido, String nombre, LocalDate fechaNac, boolean estado) {
+        this.dni = dni;
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.fechaNac = fechaNac;
+        this.estado = estado;
+    }
+
+    public int getIdAlumno() {
+        return idAlumno;
+    }
+
+    public void setIdAlumno(int idAlumno) {
+        this.idAlumno = idAlumno;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalDate getFechaNac() {
+        return fechaNac;
+    }
+
+    public void setFechaNac(LocalDate fechaNac) {
+        this.fechaNac = fechaNac;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Alumno{" + "idAlumno=" + idAlumno + ", dni=" + dni + ", apellido=" + apellido + ", nombre=" + nombre + ", fechaNac=" + fechaNac + ", estado=" + estado + '}';
+    }
 }
