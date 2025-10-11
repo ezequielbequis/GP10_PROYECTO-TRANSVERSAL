241a2e94800dca5fa73975259412603c0c980b9d
NataliaDave <nataliadave@gmail.com>
2025-10-11 20:33:00 -0300

    método listaMateria
    agregué método listarMateria

--- src/persistencia/materiaData.java
+++ src/persistencia/materiaData.java
@@ -5,11 +5,13 @@
 package persistencia;
 
 import entidades.Materia;
+import java.awt.List;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
@@ -108,7 +110,7 @@ public class materiaData {
             ResultSet rs = ps.executeQuery();
             if (rs.next()) {
                 materia = new Materia();
-                materia.setIdMateria(rs.getInt("id"));
+                materia.setIdMateria(rs.getInt("idMateria"));
                 materia.setNombre(rs.getString("nombre"));
                 materia.setAnio(rs.getInt("anio"));
                 materia.setEstado(true);
@@ -121,4 +123,29 @@ public class materiaData {
         }
         return materia;
     }
+
+    public List<Materia> listarMaterias() {
+
+        String sql = "SELECT idMateria, nombre, anio FROM materia WHERE estado=1";
+        ArrayList<Materia> materias = new ArrayList<>();
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Materia materia = new Materia();
+                materia.setIdMateria(rs.getInt("idMateria"));
+                materia.setNombre(rs.getString("nombre"));
+                materia.setAnio(rs.getInt("anio"));
+                materia.setEstado(true);
+
+                materias.add(materia);
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla materia");
+        }
+        return materias;
+    }
+
 }
