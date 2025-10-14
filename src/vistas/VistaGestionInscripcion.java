d219a89d8b69ebc649f672a814cd26137ace653c
Matias Antonacci <matiasgantonacci@gmail.com>
2025-10-14 18:41:08 -0300

    Tamaño de interfaz inscripcion y notas

--- src/vistas/VistaGestionInscripcion.java
+++ src/vistas/VistaGestionInscripcion.java
@@ -28,16 +28,17 @@ public class VistaGestionInscripcion extends javax.swing.JInternalFrame {
 
         setClosable(true);
         setIconifiable(true);
+        setPreferredSize(new java.awt.Dimension(600, 400));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 394, Short.MAX_VALUE)
+            .addGap(0, 588, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 274, Short.MAX_VALUE)
+            .addGap(0, 365, Short.MAX_VALUE)
         );
 
         pack();
